4693a7edd55a669c09060fa4a884f5f389b08e37
KLU5230505055 <5230505055@ogr.klu.edu.tr>
2023-12-08 11:56:45 +0300

    5230505055 Şifreleme Ödevi
    Sezer Özlem

---
+++ 5230505055 Sezer Özlem (Verdiğiniz Cümlenin Şifreli Hali).cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void Sifreleme(char *ptr, int anahtar) { //Sifreleme fonksiyonunu yazdim
+    while (*ptr != '\0') { //Dongu baslattim
+        if (isalpha(*ptr)) { //Sadece alfabedeki karakterleri kulanilmasi icin gerekli kodlamalari yaptim (Interneten yardim aldim)
+            char baslangic = isupper(*ptr) ? 'A' : 'a';
+            *ptr = (char)((( *ptr - baslangic + anahtar) % 26 + 26) % 26 + baslangic); ///Kaydirma sayisi kadar metni pozitif yonde kaydirdim
+        }
+        ptr++; 
+    }
+}
+
+void SifreCozme(char *ptr, int anahtar) { //Sifre cozme fonksiyonunu yazdim
+    while (*ptr != '\0') { //Dongu baslattim
+        if (isalpha(*ptr)) { //Sadece alfabedeki karakterlerin kulanilmasi icin gerekli kodlamalari yaptim (Interneten yardim aldim)
+            char baslangic = isupper(*ptr) ? 'A' : 'a';
+            *ptr = (char)((( *ptr - baslangic - anahtar) % 26 + 26) % 26 + baslangic); //Kaydirma sayisi kadar metni negatif yonde kaydirdim
+        }
+        ptr++;
+    }
+}
+
+int main() {
+    char metin[100]="The quick brown fox jumps over the lazy dog"; //Verdiginiz metni sifrelemek icin tanimladim
+    int anahtar = 3; //Kullanicidan ilerletme sayisini almak icin int degiskenini kullandim
+
+
+    printf("Kaydirma Anahtar Sayisini Giriniz: "); //Ilerletme sayisi icin ekrana "Kaydirma Anahtar Sayisini Giriniz: " ifadesini yazdirdim
+    scanf("%d", &anahtar); //Kulanicidan ilerletme sayisini  aldim
+
+    Sifreleme(metin, anahtar); //Sifreleme fonksiyonunu baslattim
+    printf("\nSifrelenmis Metin: %s\n", metin); //Ekrana "Sifrelenmis Metin: " yazdirdim
+
+    SifreCozme(metin, anahtar); //Sifre cozme fonksiyonunu baslattim
+    printf("Sifresi Cozulmus Metin: %s\n", metin); //Ekrana "Sifresi Cozulmus Metin: " yazdirdim
+
+    return 0;
+}
+
+
+
+
